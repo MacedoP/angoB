77b0df8aa69d909ea12a5574ac3f8e7889caeb05
Macedo Francisco <macedolb53@gmail.com>
2024-10-24 18:26:57 +0200

    feat:add user can search by barber

--- angobarber/app/_components/footer.tsx
+++ angobarber/app/_components/footer.tsx
@@ -1,15 +1,15 @@
-import { Card, CardContent } from "./ui/card";
+import { Card, CardContent } from "./ui/card"
 
 const Footer = () => {
-    return ( 
-        <footer>
-        <Card className="p-[10px]">
-               <CardContent className="p-[10px] text-center text-sm">
-                    © 2024 Copyright Francisco Macedo
-               </CardContent>
-             </Card>
-      </footer>
-     );
+  return (
+    <footer className="mt-6">
+      <Card className="p-[10px]">
+        <CardContent className="p-[10px] text-center text-sm">
+          © 2024 Copyright Francisco Macedo
+        </CardContent>
+      </Card>
+    </footer>
+  )
 }
- 
-export default Footer;
+
+export default Footer
--- angobarber/app/page.tsx
+++ angobarber/app/page.tsx
@@ -1,12 +1,10 @@
-import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
-import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
 import Banner from "./_components/banner"
 import Agendamento from "./_components/banner-agendamento"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/barbershop-items"
 import Filter from "./_components/filter-service"
+import Search from "./_components/search"
 
 const Home = async () => {
   
@@ -26,20 +24,18 @@ const Home = async () => {
       {/******************** Message Abaixo do Header *****************/}
       <div className="p-5">
         <h1 className="text-xl font-bold">Ola , Macedo</h1>
-        <p>Segunda-feira, 08 de agosto</p>
+        <p className="mt-1">Segunda-feira, 08 de agosto</p>
+
 
         {/******************* Input De Pesquisa ********************/}
-        <div className="flex items-center justify-between gap-2 mt-6">
-          <Input placeholder="Faca sua pesquica..." />
-
-          <Button>
-            <SearchIcon />
-          </Button>
-        </div>
+       <div className="mt-4">
+          <Search/>
+       </div>
 
         <div className="mt-6">
           <Filter/>
         </div>
+
 
         {/********************** BANNER NA HOME PAGE ******************/}
         <div className="relative mt-6">
