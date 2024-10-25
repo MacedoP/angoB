40a46e520b3c24cba6ec2e53cc11414892b7f444
Macedo Francisco <macedolb53@gmail.com>
2024-10-25 15:20:30 +0200

    feat: only authenticated user can booking

--- angobarber/app/_components/service-item.tsx
+++ angobarber/app/_components/service-item.tsx
@@ -139,7 +139,6 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
 
       await createBooking({
         serviceId: service.id,
-        userId: (data?.user as any).id,
         date: newDate,
       })
       toast.success("Reserva criada com sucesso!")
