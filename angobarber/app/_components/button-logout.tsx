196f437238b9a9c2f0bd5fa271711c7dd51798a4
Macedo Francisco <macedolb53@gmail.com>
2025-01-14 14:58:01 +0100

    Update button-logout.tsx

--- angobarber/app/_components/button-logout.tsx
+++ angobarber/app/_components/button-logout.tsx
@@ -1,6 +1,7 @@
 import { LogInIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { signOut, useSession } from "next-auth/react"
+import Link from "next/link"
 
 const ButtonOut = () => {
   const { data } = useSession()
@@ -9,9 +10,16 @@ const ButtonOut = () => {
   return (
     <div className="mb-4 flex w-full justify-start gap-2 border-b border-solid py-5">
       {data?.user && (
-        <Button variant="ghost" onClick={handleSignOut}>
-          <LogInIcon size={18} className="gap-2" />
-          Sair da Conta
+        <Button
+          variant="ghost"
+          onClick={handleSignOut}
+          className="justify-start"
+          asChild
+        >
+          <Link href="/">
+            <LogInIcon size={18} className="gap-2" />
+            Sair da Conta
+          </Link>
         </Button>
       )}
     </div>
